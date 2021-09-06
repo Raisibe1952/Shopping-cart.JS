d0039a1bd1d894c8911f11f8cf46fbf730aab7e2
Raisibe1952 <raisibe.gol@gmail.com>
2021-09-06 16:19:52 +0200

    added new files

---
+++ products.js
@@ -0,0 +1,24 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Large pizza",
+      desc : "large pizza with 5x toppings and a free side. 2 meat toppings and 3 or less other toppings.",
+      img : "pizzalarge.jpg",
+      price : 60,
+    },
+    124: {
+      name : "Medium pizza",
+      desc : "medium pizza with 4x toppings. 1 meat topping and 4 or less other toppings.",
+      img : "pizzalarge.jpg",
+      price : 80,
+    },
+    125: {
+      name : "Small pizza ",
+      desc : "small pizza with 3x toppings. 1 meat topping and 3 or less other toppings.",
+      img : "pizzalarge.jpg",
+      price : 120,
+    },
+    
+   
+  };
+  
